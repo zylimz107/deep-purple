43b35718a17ac98879219037ce2ea58bf39e04ff
zylimz107 <zylimz@hotmail.com>
2025-01-23 14:03:01 +0800

    Delete backend directory
    Unescessary for amplify

--- backend/backend/src/main/java/com/deeppurple/backend/repository/EmotionCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.deeppurple.backend.repository;
-
-import com.deeppurple.backend.entity.EmotionCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EmotionCategoryRepository extends JpaRepository<EmotionCategory, Long> {
-    List<EmotionCategory> findByModelId(Long modelId);
-}
