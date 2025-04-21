c58b9133a7d9abfa35a4bc6c13c44b4455f3bcf4
zylimz107 <zylimz@hotmail.com>
2025-04-21 21:03:53 +0800

    added security

--- deep-purple-frontend/src/api.jsx
+++ deep-purple-frontend/src/api.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import useAuthToken from '@/UseAuth';
 
-// const API_BASE_URL = 'https://purpleproj.click/emotion';
-// const API_MOD_URL = 'https://purpleproj.click/models';
-// const API_COM_URL = 'https://purpleproj.click/communications';
+const API_BASE_URL = 'https://purpleproj.click/emotion';
+const API_MOD_URL = 'https://purpleproj.click/models';
+const API_COM_URL = 'https://purpleproj.click/communications';
 
-const API_BASE_URL = "http://localhost:8080/emotion";
-const API_MOD_URL = "http://localhost:8080/models";
-const API_COM_URL = "http://localhost:8080/communications";
+// const API_BASE_URL = "http://localhost:8080/emotion";
+// const API_MOD_URL = "http://localhost:8080/models";
+// const API_COM_URL = "http://localhost:8080/communications";
 
 const useEmotionApi = () => {
   const accessToken = useAuthToken();
